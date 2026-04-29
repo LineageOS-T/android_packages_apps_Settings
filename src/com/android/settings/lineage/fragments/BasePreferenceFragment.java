Project-Legacy-UI/S

Settings: Implements spoofing features
Inspir: https://github.com/AxionAOSP/android_packages_apps_Personalizations

Signed-off-by: Rimuru <paleloebarbershop@gmail.com>
Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

src/com/android/settings/fragments/BasePreferenceFragment.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2025 AxionOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.fragments;
+
+import android.os.Bundle;
+import androidx.annotation.XmlRes;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.SearchIndexProviderHolder;
+
+public abstract class BasePreferenceFragment extends SettingsPreferenceFragment implements SearchIndexProviderHolder {
+
+    @XmlRes
+    private final int prefResId;
+
+    public BasePreferenceFragment(@XmlRes int prefResId) {
+        this.prefResId = prefResId;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(prefResId);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.LEGACY;
+    }
+
+    @Override
+    public BaseSearchIndexProvider getSearchIndexProvider() {
+        return new BaseSearchIndexProvider(prefResId) {};
+    }
+}

LineageOS-T/android_packages_apps_Settings

src/com/android/settings/lineage/fragments/BasePreferenceFragment.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2025 AxionOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.lineage.fragments;
+
+import android.os.Bundle;
+import androidx.annotation.XmlRes;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.SearchIndexProviderHolder;
+
+public abstract class BasePreferenceFragment extends SettingsPreferenceFragment implements SearchIndexProviderHolder {
+
+    @XmlRes
+    private final int prefResId;
+
+    public BasePreferenceFragment(@XmlRes int prefResId) {
+        this.prefResId = prefResId;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(prefResId);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.LINEAGE;
+    }
+
+    @Override
+    public BaseSearchIndexProvider getSearchIndexProvider() {
+        return new BaseSearchIndexProvider(prefResId) {};
+    }
+}